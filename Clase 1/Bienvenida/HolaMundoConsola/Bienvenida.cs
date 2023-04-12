fed0f0cdf215fe6031fea747c78e4814bb488bb1
hugo mansilla <_Hugo2011@hotmail.com.ar>
2023-04-11 21:45:25 -0300

    Primer commit Clase 1

---
+++ Clase 1/Bienvenida/HolaMundoConsola/Bienvenida.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundoConsola
+{
+    internal class Bienvenida
+    {
+    
+
+        public void DarBienvenida()
+        {
+            Console.WriteLine("Bienvenidos a CoderHouse!");
+
+            string sNombre;
+            int iEdad;
+            int iCont = 0;
+
+            while (iCont < 3)
+            {
+                Console.WriteLine("Ingrese su nombre:");
+                sNombre = Console.ReadLine();
+
+                Console.WriteLine("Ingrese su edad:");
+                iEdad = int.Parse(Console.ReadLine());
+
+                if (iEdad >= 18)
+                {
+                    Console.WriteLine("Mayor de edad");
+                }
+                else
+                {
+                    Console.WriteLine("Menor de Edad");
+                }
+                iCont++;
+            }
+        }
+
+    }
+}
---
+++ Clase 1/Bienvenida/HolaMundoConsola/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HolaMundoConsola;
+using System.Runtime.CompilerServices;
+
+Bienvenida bBienvenida = new Bienvenida();
+bBienvenida.DarBienvenida();
+
+
+//Console.WriteLine("Bienvenidos a CoderHouse!");
+
+//string sNombre;
+//int iEdad;
+//int iCont=0;
+//Console.WriteLine("Ingrese su nombre:");
+//sNombre = Console.ReadLine();
+
+//Console.WriteLine("Ingrese su edad:");
+//iEdad = int.Parse(Console.ReadLine());
+
+//if (iEdad >= 18)
+//{
+//    Console.WriteLine("Mayor de edad");
+//}
+//else
+//{
+//    Console.WriteLine("Menor de Edad");
+//}
+
+
+//for (int i = 0; i < 3; i++)
+//{
+//    Console.WriteLine("Ingrese su nombre:");
+//    sNombre = Console.ReadLine();
+
+//    Console.WriteLine("Ingrese su edad:");
+//    iEdad = int.Parse(Console.ReadLine());
+
+//    if (iEdad >= 18)
+//    {
+//        Console.WriteLine("Mayor de edad");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Menor de Edad");
+//    }
+//}
+
+
+
+
+// Comentario
+
+/*  
+ Comentario
+ Comentario
+ Comentario
+ 
+ */
+/*
+while (iCont<3)
+{
+    Console.WriteLine("Ingrese su nombre:");
+    sNombre = Console.ReadLine();
+
+    Console.WriteLine("Ingrese su edad:");
+    iEdad = int.Parse(Console.ReadLine());
+
+    if (iEdad >= 18)
+    {
+        Console.WriteLine("Mayor de edad");
+    }
+    else
+    {
+        Console.WriteLine("Menor de Edad");
+    }
+    iCont++;
+}
+*/
+
+
+
+Console.ReadKey();
